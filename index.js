014f642351964cf71afaa7f26056fabdca7fc721
Alejandro Corredor <aecorredor93@gmail.com>
2017-08-20 15:46:50 -0400

    finish google oauth flow, add prod keys and set up new keys export

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoUri);
+
 const app = express();
+
+// Tell express to use cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+// Tell passport to use cookies
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,14 @@ module.exports = (app) => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    // logout() is automatically added to the request object by passport
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
