0e7eade5c5ab39c54cf5a66d9ba0c6d272f9fcc0
Alejandro Corredor <aecorredor93@gmail.com>
2017-09-02 15:35:40 -0400

    finish section 7

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
   app.get('/api/logout', (req, res) => {
     // logout() is automatically added to the request object by passport
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
