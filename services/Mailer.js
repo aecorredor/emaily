b96783c7ed3a94bd4204f7d7c4ff07e1ef65c1c1
Alejandro Corredor <aecorredor93@gmail.com>
2017-10-22 22:22:56 -0400

    finish section 10: mongoose for survey creation

---
+++ services/Mailer.js
@@ -0,0 +1,51 @@
+const sendgrid = require('sendgrid');
+const keys = require('../config/keys');
+
+const helper = sendgrid.mail;
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = Mailer.formatAddresses(recipients);
+
+    this.addContent(this.body); // addContent() extends from helper.Mail
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => personalize.addTo(recipient));
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request);
+    return response;
+  }
+
+  static formatAddresses(recipients) {
+    return recipients.map(({ email }) => new helper.Email(email));
+  }
+}
+
+module.exports = Mailer;
