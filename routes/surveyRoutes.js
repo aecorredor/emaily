c34b2861d53dd5f9aba15887a6e55a76339f8f8c
Alejandro Corredor <aecorredor93@gmail.com>
2017-11-13 22:07:03 -0500

    finish course, users can now see all surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,12 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
+
+    res.send(surveys);
+  });
+
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
@@ -67,7 +73,7 @@ module.exports = (app) => {
       body,
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
-      dataSent: Date.now()
+      dateSent: Date.now()
     });
 
     // Great place to send the email
